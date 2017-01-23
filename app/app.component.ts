0f8dac237a668c9efd7804bbdbf25a0c1b299887
atbtm <atbtmmm@gmail.com>
2017-01-23 16:04:37 -0500

    Change template to multi-line. Should not change UI

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template:
-	<h1>{{title}}</h1>
-	<h2>{{hero.name}} details!</h2>
-	<div><label>id: </label>{{hero.id}}</div>
-	<div><label>name: </label>{{hero.name}}</div>
+  template:`
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>{{hero.name}}</div>
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  hero: Hero = {
-  id: 10000,
-  name: 'Windstorm'
-};
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 10000,
+		name: 'Windstorm'
+	};
 }
 export class Hero {
   id: number;
