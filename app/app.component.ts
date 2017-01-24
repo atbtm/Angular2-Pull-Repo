60ee797c01c8e349226ba77464148561f94c8c84
atbtm <atbtmmm@gmail.com>
2017-01-24 16:19:10 -0500

    refactoring input val

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 	<h2>My Heroes</h2>
 	<ul class="heroes">
 		<li *ngFor="let hero of heroes">
-			<span class="badge">{{hero.id}}</span>{{hero.name}}
+			<span class="badge">{{hero.id}}</span> {{hero.name}}
 		</li>
 	</ul>
   `
